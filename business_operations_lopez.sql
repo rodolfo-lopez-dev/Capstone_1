e5af5501904f3e61ab9c7bec4d045f743fe4a3a9
Rodolfo <rlopez1@appdev.yearup.org>
2024-10-16 10:38:04 -0500

    finished erd diagram and now working on creation of tables in my business

---
+++ business_operations_lopez.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE Customer (
+    Customer_ID INT AUTO_INCREMENT PRIMARY KEY,
+    First_Name VARCHAR(50) NOT NULL,
+    Last_Name VARCHAR(50) NOT NULL,
+    Email VARCHAR(100),
+    Phone_Number VARCHAR(15) NOT NULL
+);
+
