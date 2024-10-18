6c0ece12d0445614c9b501a99ec99506df023fe5
Rodolfo <rlopez1@appdev.yearup.org>
2024-10-17 20:26:32 -0500

    added valuable insight on ethnic and income

--- market_research.sql
+++ market_research.sql
@@ -10,6 +10,13 @@ WHERE '60629';
 
 # shows me schools, and the amount of students that are associated with the entire school
 
+
+SELECT COUNT(*) 
+FROM dataforcps
+WHERE Student_Count_Total IS NULL;
+
+# ensuring the accuracy of student count by looking for missing/nulls values
+
 SELECT COUNT(*) 
 FROM dataforcps
 WHERE Zip = '60629' AND Is_High_School = 'True';
@@ -21,13 +28,13 @@ FROM dataforcps
 WHERE (Is_Middle_School = 'True' OR Is_High_School = 'True') AND Zip = '60629';
 
 /* total number of students in middle and high school within the area code with the potential market for
-a sports complex that would benefit the people in the area */
+the age groups that are likely to go to a sport complex in the area */
 
 SELECT `FACILITY TYPE`, COUNT(*) AS facility_count
 FROM market_research.park_research_data
 GROUP BY `FACILITY TYPE`;
 
-# shows the facilities in chicago that are indoor/outdoor and leads to realize a need for more indoor facilites
+# shows the facilities in chicago that are indoor/outdoor and results demonstrates a need for a sports complex that will be indoors
 
 
 SELECT `Age Group`, SUM(`60629 Value`) AS population
@@ -36,7 +43,7 @@ WHERE `Age Group` IN ('10 to 14 years', '15 to 17 years', '18 and 19 years', '20
                       '22 to 24 years', '25 to 29 years', '30 to 34 years', '35 to 39 years')
 GROUP BY `Age Group`;
 
-# found age group most likely to use a sport complex
+# found age group most likely to use a sport complex for women
 
 SELECT `Age Group`, SUM(`60629 Value`) AS population
 FROM market_research.male_zipcode
@@ -47,7 +54,17 @@ GROUP BY `Age Group`;
 # similar but now the male population 
 
 
+SELECT 
+    Zip,
+    SUM(Student_Count_Hispanic) AS total_hispanic,
+    SUM(Student_Count_Black) AS total_black,
+    SUM(Student_Count_White) AS total_white,
+    SUM(Student_Count_Low_Income) AS total_low_income
+FROM dataforcps
+GROUP BY Zip
+ORDER BY total_low_income DESC;
 
-
+/* the results show that in the area there's a high level of low-income students that would benefit 
+from an affordable and accessible facility such as a sports complex  */ 
 
 
